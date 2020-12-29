06fede25b43ddf3d4ea152c4380b72521a361f03
Indi <indiboteju@Indis-MBP.lan>
2020-12-28 21:24:24 -0800

    Updates

---
+++ precache-manifest.95e797058f6addc5eace8df8c6830845.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f15a5957e6ac306b165a843b2b73b5",
+    "url": "/react-product-manager/index.html"
+  },
+  {
+    "revision": "f091a9748fc7b001a2c2",
+    "url": "/react-product-manager/static/css/main.3b9ac592.chunk.css"
+  },
+  {
+    "revision": "35ce47a42bf91dd30923",
+    "url": "/react-product-manager/static/js/2.bae1f6ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-product-manager/static/js/2.bae1f6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f091a9748fc7b001a2c2",
+    "url": "/react-product-manager/static/js/main.08c05157.chunk.js"
+  },
+  {
+    "revision": "0038443b89ef1b423d79",
+    "url": "/react-product-manager/static/js/runtime-main.5188a56b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-product-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
